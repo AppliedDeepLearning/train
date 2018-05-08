688e118bdfcf8645c94f394b1c652f07cebddb07
Ravindra Marella <mv.ravindra007@gmail.com>
2018-05-09 00:15:58 +0530

    0.0.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ name = 'train'
 
 setup(
     name=name,
-    version='0.0.2',
+    version='0.0.3',
     description=long_description.splitlines()[0],
     long_description=long_description,
     long_description_content_type='text/markdown',
